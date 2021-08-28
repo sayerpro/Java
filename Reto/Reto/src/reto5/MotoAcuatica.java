24424564194d8282afd00caa99e2e1b423af51ee
TheSrSmith <sayerpro.op7@gmail.com>
2021-08-28 12:03:50 -0500

    Resolucion reto 5 que bobada fue mas la pereza de leerlo que la demora en hacerlo

---
+++ Reto/Reto/src/reto5/MotoAcuatica.java
@@ -0,0 +1,38 @@
+package reto5;
+
+public class MotoAcuatica {
+
+    private String Id;
+    private String Modelo;
+    private String Marca;
+
+    public MotoAcuatica(String id, String modelo, String marca) {
+        setId(id);
+        setModelo(modelo);
+        setMarca(marca);
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+}
---
+++ Reto/Reto/src/reto5/Persona.java
@@ -0,0 +1,38 @@
+package reto5;
+
+public class Persona {
+
+    private String Cedula;
+    private int Edad;
+    private String Nombre;
+
+    public Persona(String cedula, int edad, String nombre) {
+        setCedula(cedula);
+        setEdad(edad);
+        setNombre(nombre);
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String cedula) {
+        Cedula = cedula;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+}
